49580a7dd20de5bb56614b52c6e909248228a3f6
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-04-20 18:08:56 +0530

    First commit

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,10 +8,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+struct capsuleText: View {
+    
+    var text: String
+    
+    var body: some View{
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.blue)
+            .background(Color.yellow)
+            .clipShape(Capsule())
+        
+        
     }
+}
+
+struct Title: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.blue)
+            .padding()
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+extension View{
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+struct ContentView: View {
+    
+    @State private var isRed = false
+    
+    let text1 = Text("First text")
+    let text2 = Text("First text")
+    
+    var body: some View {
+        VStack(spacing: 15){
+            capsuleText(text: "First Text")
+            capsuleText(text: "Second Text")
+            
+            Text("hello world")
+                .modifier(Title())
+            Text("SwiftUI")
+            .titleStyle()
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
